7f559ee81fc3e5da79b24c4a15878b97536076b7
SalazarSlytherin13 <vanyatripathig1484@gmail.com>
2026-08-02 00:02:07 +0530

    3_dataTypeConversion.js

---
+++ 3_dataTypeConversion.js
@@ -0,0 +1,41 @@
+// let score = "33";          //string to number
+// console.log(typeof score);
+
+// let scoreNumber = Number(score);
+// console.log(scoreNumber);
+// console.log(typeof scoreNumber);
+
+
+// let jersey = 0;
+// console.log(typeof jersey)
+
+// let jerseyBool = Boolean(jersey);  //number to boolean
+// console.log(jerseyBool);           //24 =>true  0 =>false
+// console.log(typeof jerseyBool);
+
+
+// let name = "Eddard"
+// console.log(typeof name);
+
+// let nameBool = Boolean(name);  //string to boolean
+// console.log(nameBool);         //"Eddard" =>true  "" =>false
+// console.log(typeof nameBool);
+
+
+// let code = "33abc"
+// console.log(code);
+
+// let codeNumber = Number(code);
+// console.log(codeNumber);
+// console.log(typeof codeNumber);  //gives NaN => Not a number
+
+let state = null
+console.log( typeof state);
+
+let stateBool = Boolean(state);    //null to boolean
+console.log(stateBool);            //null =>false
+console.log(typeof stateBool);
+
+
+
+
