1671da334cd92e787d50c8d43ff285ee2dab89a3
SalazarSlytherin13 <vanyatripathig1484@gmail.com>
2026-08-02 15:42:13 +0530

    4_dataTypeOperations.js

---
+++ 4_dataTypeOperations.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello"
+let Str2 = " Everybody"
+let str3 = str1 + Str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");      //not preferred
+// console.log(-true);
+// console.log(+false);
+// console.log(-false);
+
+let scoreCard = 99;
+console.log(scoreCard);
+scoreCard++;
+console.log(scoreCard);
+
+let score = 99;
+console.log(score);
+++score;
+console.log(score);
+
